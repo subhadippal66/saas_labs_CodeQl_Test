2bc607d0a305657c6b7af0fcefbdd3c3b296037f
subhadippal66 <subhadippal66@gmail.com>
2022-01-20 20:39:42 +0530

    first commit

---
+++ 1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>GITHUB</h1>
+</body>
+</html>
+
+<script src="https://cdn.justcall.io/main/assets/js/bootstrap.min.js"></script>
+<script src="https://cdn.justcall.io/main/assets/js/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+
+
+<?php
+
+
+
+
+
+?>
